7cdfc863530db396800e68be8298cb43b1f48ae4
Sergyung <sergbest@mail.ru>
2022-09-05 14:51:04 +0300

    modification pageDivision

--- pageDivision.php
+++ pageDivision.php
@@ -1,3 +1,10 @@
+<?php
+	session_start();
+
+	require 'connectBd.php';
+			
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,15 +13,33 @@
 	<title>Редактирование подразделений</title>
 </head>
 <body>
-	<form>
+	<form action="addDivision.php" method="post">
 		<p>
 			<label>Подразделение</label>
 			<input type="text" name="division">
 		</p>
 		<p>
-			<input type="button" name="button" value="Записать">
+			<!-- <input type="button" name="button" > -->
+			<input type="submit" name="enter_ship" value="Записать">
 		</p>
 	</form>
 	<p><a href="adminPanel.php">Возврат назад</a></p>
+
+	<br>
+		<div class="list_auto"> 
+		<div class="autos">
+		<table>	
+			<?php
+				$sql_div = "SELECT division FROM divisions";
+				$result = mysqli_query($link, $sql_div) or die( mysqli_error($link) );
+				$i = 1;			
+				while ($row_division = mysqli_fetch_assoc($result)) {
+					echo '<tr><td>'.$i.'</td><td><a href = "#">'.$row_division['division'].'</a></td></tr>';
+				$i++;
+				}
+			?>
+		</table>
+		</div>
 </body>
 </html>
+
