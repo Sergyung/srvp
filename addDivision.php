4d733e7f38bf34800666ab028538d9438b48fdf5
Sergyung <sergbest@mail.ru>
2022-09-05 14:49:03 +0300

    add addDivision.php

---
+++ addDivision.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+	require 'connectBd.php';
+
+	if (!empty($_POST[division])) {
+		$division = htmlspecialchars($_POST['division']);
+	} else {
+		die('Вы забыли ввести название подразделения. <a href="pageDivision.php">RETUR</a>');
+	}
+
+	$query = "INSERT INTO divisions (division) VALUES('$division')";
+	$result = mysqli_query($link, $query) or die(mysqli_error($link));
+	
+	if (isset($result)) {
+		echo '<h4>Запрос на добавление судна в базу данных прошел. <a href="pageDivision.php">Вернуться</a></h4>';
+	}
+		
+?>
