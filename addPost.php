87e24f4f63210042e08c32842fca95a0d264ed11
Sergyung <sergbest@mail.ru>
2022-09-06 09:31:04 +0300

    add post processing

---
+++ addPost.php
@@ -0,0 +1,30 @@
+<?php
+
+	session_start();
+
+	require 'connectBd.php';
+
+	if (!empty($_POST['posts'])) {
+		$posts = htmlspecialchars($_POST['posts']);
+	} else {
+		die('Вы забыли ввести название должности. <a href="pagePost.php">Вернуться на предыдущую страницу</a>');
+
+	}
+
+	// if (!empty($_POST['project_number'])) {
+	// 	$project_number = htmlspecialchars($_POST['project_number']);
+	// } else {
+	// 	die('Вы забыли ввести проект судна. <a href="pagePost.php">Вернуться на предыдущую страницу</a>');
+	// }
+
+	$idDivision = ($_POST['idDivision']);
+
+	$sql_ship = "INSERT INTO positions (post, idDivision)  VALUES ('$posts','$idDivision')";
+
+	$result = mysqli_query($link, $sql_ship) or die( mysqli_error($link) );
+
+	if (isset($result)) {
+		echo '<h4>Запрос на добавление должности в базу данных прошел. <a href="pagePost.php">Вернуться на предыдущую страницу</a></h4>';
+	}
+
+?>
