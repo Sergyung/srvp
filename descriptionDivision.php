4ac7e3ec1465ae231474ce485d8b7260677135ff
Sergyung <sergbest@mail.ru>
2022-10-03 16:35:14 +0300

    modified descriptionDivision.php

--- descriptionDivision.php
+++ descriptionDivision.php
@@ -8,17 +8,17 @@ $query1 = "SELECT division FROM divisions WHERE id = '$divi'" ;
 
 $result1 = mysqli_query($link, $query1);
 $row1 = $result1->fetch_assoc();
-//$row = mysqli_fetch_assoc($result);
+
 echo '<h2>'.$row1['division'].'</h2>';
 
-$query = "SELECT post, lastName FROM positions LEFT JOIN users ON users.postID = positions.id WHERE idDivision = '$divi' ORDER BY positions.id" ; 
+$query = "SELECT post, lastName, firstName, middleName FROM positions LEFT JOIN users ON users.postID = positions.id WHERE idDivision = '$divi' ORDER BY positions.id" ; 
 
 $result = mysqli_query($link, $query);
-echo '<table border="1">';
+echo '<table>';
 echo '<tr><th>№<br>п/п</th><th>Название должности</th><th>Фамилия<br> и инициалы</th></tr>'; 
 $i=1;
 while($row = $result->fetch_assoc()) {
-	echo '<tr><td>'.$i.'</td><td>'.$row['post'].'</td><td>'.$row['lastName'].'</td></tr>'; 
+	echo '<tr><td>'.$i.'</td><td>'.$row['post'].'</td><td>'.$row['lastName'].' '.substr($row['firstName'], 0, 2).'.'.substr($row['middleName'], 0, 2).'.'.'</td></tr>'; 
  	$i++;
 }
 
