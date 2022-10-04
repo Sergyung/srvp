3ec00fcc5e5658ad5185e224424dd3d2d526f80d
Sergyung <sergbest@mail.ru>
2022-10-04 16:35:35 +0300

    updating the page functionality

--- pageUser.php
+++ pageUser.php
@@ -1,87 +1,32 @@
 <?php
-	session_start();
 
-	require 'connectBd.php';
-			
+require_once("connectBd.php");
+
+$user = $_GET['user'];
+
+$query1 = "SELECT * FROM users WHERE id = '$user'" ; 
+
+$result1 = mysqli_query($link, $query1);
+$row1 = $result1->fetch_assoc();
+
+echo '<h2>'.$row1['lastName'].' '.$row1['firstName'].' '.$row1['middleName'].'</h2>';
+
+// $query = "SELECT * FROM positions LEFT JOIN users ON users.postID = positions.id WHERE idDivision = '$divi' ORDER BY positions.id" ; 
+
+// $result = mysqli_query($link, $query);
+// echo '<table>';
+// echo '<tr><th>№<br>п/п</th><th>Название должности</th><th>Фамилия<br> и инициалы</th></tr>'; 
+// $i=1;
+// while($row = $result->fetch_assoc()) {
+
+// 	if ($row[occupied] == 1) {$text = '<a href="#">'.$row['lastName'].' '.substr($row['firstName'], 0, 2).'.'.substr($row['middleName'], 0, 2).'.</a>';} else {$text = '';};
+	
+// 	echo '<tr><td>'.$i.'</td><td><a href="#">'.$row['post'].'</a></td><td>'.$text.'</td></tr>'; 
+
+//  	$i++;
+// }
+
+// echo '</table>';
+echo '<p><a href="listDivisions.php">Возврат назад</a></p>';
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Редактирование сотрудников</title>
-</head>
-
-<script src="jquery.main.js"></script>
-
-<script>
-    function getPost(val){
-        $.ajax({
-            type: "POST",
-            url: "getPost.php",
-            data: 'division_id='+val,
-            success: function(data){
-                $("#post-list").html(data);
-            }
-        });
-    }
-</script>
-
-<body>
-	<form action="addUser.php" method="post">
-		<p>
-			<label>Фамилия</label>
-			<input type="text" name="lastName">
-		</p>
-		<p>
-			<label>Имя</label>
-			<input type="text" name="firstName">
-		</p>
-		<p>
-			<label>Отчество</label>
-			<input type="text" name="middleName">
-		</p>
-		<p>
-			<label>Дата рождения</label>
-			<input type="date" name="dateBirth">
-		</p>
-		<p>
-			<label>Дата принятия на работу</label>
-			<input type="date" name="dateJob">
-		</p>
-
-			<label>Подразделение</label>
-			<select name="division" id="division-list" onchange="getPost(this.value);">
-                <option value disabled selected>Выбери лодразделение</option>
-                <?php
-                	$query = "SELECT * FROM divisions"; 
-					$result = mysqli_query($link, $query);
-					 
-					while($row = $result->fetch_assoc())
-					{
-					    echo '<option value ='.$row['id'].'>'.$row['division'].'</option>';
-					}
-				?>
-			</select>
-		
-		<p>
-			<label>Должность</label>
-            <select name="posts" id="post-list">
-                <option value="">Выберите должность</option>
-            </select>
-		</p>
-		<p>
-			<label>Дата назначения</label>
-			<input type="date" name="datePost">
-		</p>	
-		<p>
-			<input type="submit" name="enter_ship" value="Записать">
-		</p>
-	</form>
-	<p><a href="adminPanel.php">Возврат назад</a></p>
-
-	<br>
-		
-</body>
-</html>
